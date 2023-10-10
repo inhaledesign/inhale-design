c6ed3d26361979b889770869187912fe925b3cd3
Bob Liberatore <bob@inhale.design>
2023-10-10 16:54:08 -0600

    Make mobile responsive, move social links to top nav

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,7 @@ module.exports = {
                     ...require("daisyui/src/theming/themes")["[data-theme=light]"],
                     "primary": "rgb(125 211 252)", // Sky 300
                     "primary-content": "#fff",
+                    "secondary": "#3b82f6",
                     "accent": "#fef08a",
                     "neutral": "#fcf8ff"
                 }
